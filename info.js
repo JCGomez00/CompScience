122030937e5743e38ac0e9da27251dc41d278cc1
jcgomez00 <jegomez.sp@mysummitps.org>
2015-11-10 21:54:00 +0000

    modify

--- info.js
+++ info.js
@@ -1,15 +1,41 @@
 var item1 = document.getElementById("item1");
 var item2 = document.getElementById("item2");
+var item3 = document.getElementById("item3");
+var item4 = document.getElementById("item4");
 var info1 = document.getElementById("info1");
 var info2 = document.getElementById("info2");
+var info3 = document.getElementById("info3");
+var info4 = document.getElementById("info4");
+var info5 = document.getElementById("info5");
+var info6 = document.getElementById("info6");
+var info7 = document.getElementById("info7")
+var info8 = document.getElementById("info8");
+var info9 = document.getElementById("info9");
+var info10 = document.getElementById("info10");
+var info11 = document.getElementById("info11");
+var info12 = document.getElementById("info12");
 
 item1.addEventListener("click", function(){
-    info1.innerHTML = "Some stuff";
-    info2.innerHTML = "some other stuff";
+    info1.innerHTML = "This require a lot of patients";
+    info2.innerHTML = "You have to very quiet when it comes to fishing. If you speak to loud you will scare away the fish.";
+    info3.innerHTML = "This sport is you,your rod, and nature.";
 });
                        
 item2.addEventListener("click", function(){
-    info1.innerHTML = "Item 2 stuff";
-    info2.innerHTML = "Item 2 other stuff";
+    info4.innerHTML = "Your rod and tackle are the most important things you need to be a successful fisherman.";
+    info5.innerHTML = "Your rod is your friend. It will fight will you when you get hooked with a fish.";
+    info6.innerHTML = "Your tackle is the hardest thing to choose because not all fish like the same things. You have to choose your tackle depending what kind of fish you are going after for.";
+    
 });
-                       
+
+  item3.addEventListener("click", function(){
+    info7.innerHTML = "When it comes to fishing it is 90% luck and 10% skills.";
+    info8.innerHTML = "Luck in fishing is your best friend. If you do not have luck you will not catch any fish.";
+    info9.innerHTML = "Skills is much needed becauase you never know how the fish is going to fight and you have to know how to react to it.";
+    
+});
+item4.addEventListener("click", function(){
+    info10.innerHTML = "Knowledge is the key to everything even in fishing.";
+    info11.innerHTML = "If you have knowledge about fishing there is not any fish that can get away from you.";
+    info12.innerHTML ="By having this you will know diffent techiques,tackles,and fishing spots"
+    });
